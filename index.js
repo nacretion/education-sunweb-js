6b701d84c2c86f4ec08eb29226fff38164b63f68
nacretion <ananjev.nikita2017@ya.ru>
2023-11-30 17:27:44 +0500

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+const staffs = [
+    {
+        id: 1,
+        name: "John",
+        age: 30,
+        gender: "male",
+        salary: 5000,
+        married: false,
+        skills: ["html", "css", "js"],
+        employment_at: "2020-01-01"
+    },
+    {
+        id: 2,
+        name: "Jane",
+        age: 25,
+        gender: "female",
+        salary: 4000,
+        married: true,
+        skills: ["html", "css", "js", "php"],
+        employment_at: "2023-06-21"
+    },
+    {
+        id: 3,
+        name: "Bob",
+        age: 35,
+        gender: "male",
+        salary: 6000,
+        married: false,
+        skills: ["html", "css", "js", "python"],
+        employment_at: "2021-03-15"
+    },
+    {
+        id: 4,
+        name: "Alice",
+        age: 28,
+        gender: "female",
+        salary: 4500,
+        married: true,
+        skills: ["html", "css"],
+        employment_at: "2022-09-01"
+    },
+    {
+        id: 5,
+        name: "Charlie",
+        age: 40,
+        gender: "male",
+        salary: 7000,
+        married: true,
+        skills: ["html", "css", "js", "python", "java"],
+        employment_at: "2020-07-10"
+    },
+    {
+        id: 6,
+        name: "Emily",
+        age: 32,
+        gender: "female",
+        salary: 5000,
+        married: true,
+        skills: ["js", "C++"],
+        employment_at: "2023-02-28"
+    },
+    {
+        id: 7,
+        name: "David",
+        age: 29,
+        gender: "male",
+        salary: 5500,
+        married: true,
+        skills: ["html", "css", "js"],
+        employment_at: "2021-11-05"
+    },
+    {
+        id: 8,
+        name: "Sophia",
+        age: 27,
+        gender: "female",
+        salary: 4000,
+        married: true,
+        skills: ["html", "css", "js"],
+        employment_at: "2022-08-15"
+    }
+]
+
