65a865dc2496a0e7db21ef7eef462082eee8848e
nacretion <ananjev.nikita2017@ya.ru>
2023-12-04 11:28:11 +0500

    Запроси пользователей с API

--- index.js
+++ index.js
@@ -1,85 +1,27 @@
-const staffs = [
-    {
-        id: 1,
-        name: "John",
-        age: 30,
-        gender: "male",
-        salary: 5000,
-        married: false,
-        skills: ["html", "css", "js"],
-        employment_at: "2020-01-01"
-    },
-    {
-        id: 2,
-        name: "Jane",
-        age: 25,
-        gender: "female",
-        salary: 4000,
-        married: true,
-        skills: ["html", "css", "js", "php"],
-        employment_at: "2023-06-21"
-    },
-    {
-        id: 3,
-        name: "Bob",
-        age: 35,
-        gender: "male",
-        salary: 6000,
-        married: false,
-        skills: ["html", "css", "js", "python"],
-        employment_at: "2021-03-15"
-    },
-    {
-        id: 4,
-        name: "Alice",
-        age: 28,
-        gender: "female",
-        salary: 4500,
-        married: true,
-        skills: ["html", "css"],
-        employment_at: "2022-09-01"
-    },
-    {
-        id: 5,
-        name: "Charlie",
-        age: 40,
-        gender: "male",
-        salary: 7000,
-        married: true,
-        skills: ["html", "css", "js", "python", "java"],
-        employment_at: "2020-07-10"
-    },
-    {
-        id: 6,
-        name: "Emily",
-        age: 32,
-        gender: "female",
-        salary: 5000,
-        married: true,
-        skills: ["js", "C++"],
-        employment_at: "2023-02-28"
-    },
-    {
-        id: 7,
-        name: "David",
-        age: 29,
-        gender: "male",
-        salary: 5500,
-        married: true,
-        skills: ["html", "css", "js"],
-        employment_at: "2021-11-05"
-    },
-    {
-        id: 8,
-        name: "Sophia",
-        age: 27,
-        gender: "female",
-        salary: 4000,
-        married: true,
-        skills: ["html", "css", "js"],
-        employment_at: "2022-08-15"
+let staffs = []
+const staffsProxy = new Proxy(staffs, {
+    set: function (target, index, value) {
+        target[index] = value;
+        showData()
+        return true;
+    }
+});
+
+const baseGETURL = 'https://api.slingacademy.com/v1/sample-data'
+const basePOSTURL = 'https://jsonplaceholder.typicode.com'
+
+const getUsers = async (limit = 20, offset = 0) => {
+    const url = baseGETURL + `/users?offset=${offset}&limit=${limit}`
+
+    const data = await fetch(url)
+
+    const response = await data.json()
+
+    if (response.success) {
+        return response.users
     }
 ]
+}
 
 
 const tbody = document.getElementById('table-body')
@@ -88,10 +30,11 @@ const form = document.getElementById('form')
 const filter = document.getElementById('filter')
 const tableFields = [ 'id', 'name', 'skills', 'employment_at', 'gender', 'age', 'salary' ]
 
+const tableFields = ['id', 'first_name', 'last_name', 'date_of_birth', 'gender', 'state', 'street']
 const getFormatFunc = {
     default: (field) => field,
     gender: (field) => field.trim().toLowerCase() === 'male' ? 'Мужской' : 'Женский',
-    salary: (field) => field + ' ₽'
+    date_of_birth: (field) => new Date(field).toLocaleDateString()
 }
 
 const showData = (data = staffs) => {
@@ -199,4 +142,4 @@ document.addEventListener('click', (ev) => {
 
 filter.addEventListener('input', debounce(() => filterData()))
 
-showData()
+getUsers().then(users => staffsProxy.push(...users))
