29a98b975c2c32c230ea37e57083d38e38a88d86
nacretion <ananjev.nikita2017@ya.ru>
2023-12-07 12:32:54 +0500

    Запрашивай пользователей on mount

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,22 @@
 </template>
 
 <script>
+import { UsersApi } from '../ajax'
+const api = new UsersApi(
+    'https://api.slingacademy.com/v1/sample-data/users',
+    'https://jsonplaceholder.typicode.com/users'
+)
 
 export default {
   name: 'App',
   components: {
+  data() {
+    return {
+      items: [{}],
+  mounted() {
+    api.getUsers({fields: this.tableFields}).then(users => {
+      this.items = users
+    })
   }
 }
 </script>
