1c24d297a040d90701a366891379db07a1efcca0
nacretion <ananjev.nikita2017@ya.ru>
2023-12-07 18:54:54 +0500

    Создай button компонент

---
+++ src/components/base/button/button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+      class="button"
+      :class="`button--${variant}`"
+      :type="type"
+      @click="$emit('click')"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'VueButton',
+  props: {
+    type: {
+      type: String,
+      default: 'submit'
+    },
+    variant: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style src="./button.css">
+
+</style>
