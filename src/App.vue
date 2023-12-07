f359ccbf7cb88d43312762b949952d1593925cba
nacretion <ananjev.nikita2017@ya.ru>
2023-12-07 12:33:24 +0500

    Используй компонент VueTable

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
+    <VueTable :items="items" :replacements="tableFieldsReplacements" :valuesReplacements="tableValuesReplacements"></VueTable>
   </div>
 </template>
 
 <script>
+import VueTable from './components/base/table/table.vue'
 import { UsersApi } from '../ajax'
+
 const api = new UsersApi(
     'https://api.slingacademy.com/v1/sample-data/users',
     'https://jsonplaceholder.typicode.com/users'
@@ -13,9 +16,40 @@ const api = new UsersApi(
 export default {
   name: 'App',
   components: {
+    VueTable
+  },
   data() {
     return {
       items: [{}],
+      tableFields: [
+          'id',
+          'first_name',
+          'last_name',
+          'date_of_birth',
+          'gender',
+          'email',
+          'street'
+      ],
+      tableFieldsReplacements: {
+        id: '#',
+        first_name: 'Имя',
+        last_name: 'Фамилия',
+        date_of_birth: 'Дата рождения',
+        gender: 'Пол',
+        email: 'Электронная почта',
+        street: 'Улица'
+      },
+      tableValuesReplacements: {
+        default: (field) => field,
+        date_of_birth: (field) => new Date(field).toLocaleDateString('en-US'),
+        gender: (field) => field.trim().toLowerCase() === 'male' ? 'Мужской' : 'Женский',
+      }
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
   mounted() {
     api.getUsers({fields: this.tableFields}).then(users => {
       this.items = users
@@ -25,4 +59,16 @@ export default {
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+  }
+  body{
+    font-family: Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    background: rgba( 71, 147, 227, 1);
+  }
 </style>
---
+++ src/components/base/table/table.vue
@@ -0,0 +1,61 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col" v-for="column in Object.keys(items[0])" :key="column">
+          {{ replacements? replacements[column] : column }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in items" :key="item[Object.keys(item)[0]]">
+        <td v-for="key in Object.keys(item)" :key="item[key]">
+          {{ replaceValue(key, item[key]) }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'VueTable',
+  components: {
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    replacements: {
+      type: Object
+    },
+    valuesReplacements: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+    replaceValue: function (key, field) {
+      if (!this.valuesReplacements) {
+        return field
+      }
+
+      const replaceValue = this.valuesReplacements[key] || this.valuesReplacements.default
+      return replaceValue(field)
+    }
+  },
+  watch: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style src="./table.css">
+</style>
