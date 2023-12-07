8e531f6f9065fbb883ede7f7bce5e094938763f9
nacretion <ananjev.nikita2017@ya.ru>
2023-12-07 18:55:14 +0500

    Улучши input компонент

--- src/components/base/input/input.vue
+++ src/components/base/input/input.vue
@@ -1,11 +1,30 @@
 <template>
-  <input
-      class="input"
-      :type="type"
-      :placeholder="placeholder"
-      :value="value"
-      @input="$emit('input', $event.target.value)"
-  >
+  <div class="input__wrapper">
+    <div class="input__with-label">
+      <label
+          v-if="id"
+          class="input__label"
+          :for="id"
+      >
+        <slot/>
+      </label>
+      <input
+          class="input"
+          :id="id"
+          :type="type"
+          :placeholder="placeholder"
+          :value="value"
+          :checked="checked"
+          :name="name"
+          :class="{'input--error': isError}"
+          @input="$emit('input', $event)"
+          @change="$emit('input', $event)"
+      >
+    </div>
+    <div v-if="isError" class="input__error">
+      {{ errorMessage }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,6 +39,21 @@ export default {
       type: String
     },
     value: {
+      type: String
+    },
+    id: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    isError: {
+      type: Boolean
+    },
+    checked: {
+      type: Boolean
+    },
+    errorMessage: {
       type: String
     }
   },
