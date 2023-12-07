fff3cf0dfc70ac32c1b4ebbf04a43918bf2998cb
nacretion <ananjev.nikita2017@ya.ru>
2023-12-07 18:56:27 +0500

    Создай окно выбора действий

---
+++ src/components/ChooseModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <VueModal :show="show">
+    <div class="controls">
+      <VueButton @click="$emit('edit')">
+        Изменить
+      </VueButton>
+      <VueButton @click="$emit('delete')" :variant="'danger'">
+        Удалить
+      </VueButton>
+    </div>
+    <VueButton @click="$emit('close')" :variant="'secondary-sized'">
+      Закрыть
+    </VueButton>
+  </VueModal>
+
+</template>
+
+<script>
+import VueModal from "@/components/base/modal/modal.vue";
+import VueButton from "@/components/base/button/button.vue";
+
+export default {
+  name: 'ChooseModal',
+  components: {VueButton, VueModal},
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+
+}
+</script>
+<style>
+
+</style>
